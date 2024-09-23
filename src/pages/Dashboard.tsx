feb9a1c26c2178c3ada2b437c5655d2193615a0b
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-09-23 23:55:39 +0530

    Add Graph Container

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,9 @@ import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
 import userImg from "../assets/userpic.png";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
+import data from "../assets/data.json";
+import { BarChart } from "../components/Charts";
+import { BiMaleFemale } from "react-icons/bi";
 
 const dashboard = () => {
     return (
@@ -54,17 +57,50 @@ const dashboard = () => {
                     <div className="revenueChart">
                         <h2>Revenue & Transaction</h2>
                         {/* Graph here */}
+                        <BarChart
+                            data_2={[300, 144, 433, 655, 237, 755, 190]}
+                            data_1={[200, 444, 343, 556, 778, 455, 990]}
+                            title_1="Revenue"
+                            title_2="Transaction"
+                            bgColor_1="rgb(0,115,255)"
+                            bgColor_2="rgba(53,162,235,0.8)"
+                            // horizontal={true}
+                        />
                     </div>
                     <div className="dashboardCategories">
                         <h2>Inventory</h2>
-                        x
+                        <div>
+                            {data.categories.map((i) => (
+                                <CategoryItem
+                                    key={i.heading}
+                                    heading={i.heading}
+                                    value={i.value}
+                                    color={`hsl(${i.value * 3}, ${
+                                        i.value
+                                    }%, 50%)`}
+                                />
+                            ))}
+                        </div>
                     </div>
+                </section>
+
+                <section className="transactionContainer">
+                  <div className="genderChart">
+                    <h2>Gender Ratio</h2>
+                    {/* Chart */}
+                    <p>
+                      <BiMaleFemale/>
+                    </p>
+                  </div>
+
+                  {/* Table */}
                 </section>
             </main>
         </div>
     );
 };
 
+//Widget Items
 interface WidgetItemProps {
     heading: string;
     value: number;
@@ -107,6 +143,28 @@ const WidgetItem = ({
             <span style={{ color }}>{percent}%</span>
         </div>
     </article>
+);
+
+//Inventories
+interface CategoryItemProps {
+    color: string;
+    value: number;
+    heading: string;
+}
+
+const CategoryItem = ({ color, value, heading }: CategoryItemProps) => (
+    <div className="categoryItem">
+        <h5>{heading}</h5>
+        <div>
+            <div
+                style={{
+                    backgroundColor: color,
+                    width: `${value}%`,
+                }}
+            ></div>
+        </div>
+        <span>{value}%</span>
+    </div>
 );
 
 export default dashboard;
