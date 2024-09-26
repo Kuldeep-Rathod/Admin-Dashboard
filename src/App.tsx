e7039b9c32d7d56ddfe78c493c6372cbda452835
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-09-27 04:27:29 +0530

    app section completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader";
 
+
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Customers = lazy(() => import("./pages/Customers"));
 const Transaction = lazy(() => import("./pages/Transaction"));
 const Products = lazy(() => import("./pages/Products"));
 const NewProduct = lazy(() => import("./pages/management/NewProduct"));
-const ProductManagement = lazy(
-    () => import("./pages/management/ProductManagement")
-);
-const TransactionManagement = lazy(
-    () => import("./pages/management/TransactionManagement")
-);
+const ProductManagement = lazy(() => import("./pages/management/ProductManagement"));
+const TransactionManagement = lazy(() => import("./pages/management/TransactionManagement"));
 const BarCharts = lazy(() => import("./pages/charts/BarCharts"));
 const PieCharts = lazy(() => import("./pages/charts/PieCharts"));
 const LineCharts = lazy(() => import("./pages/charts/LineCharts"));
+const Stopwatch = lazy(() => import("./pages/apps/Stopwatch"));
+const Coupon = lazy(() => import("./pages/apps/Coupon"));
+const Toss = lazy(() => import("./pages/apps/Toss"));
+
 
 function App() {
     return (
@@ -35,6 +36,9 @@ function App() {
                     <Route path="/admin/chart/line" element={<LineCharts />} />
 
                     {/* Apps */}
+                    <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+                    <Route path="/admin/app/coupon" element={<Coupon />} />
+                    <Route path="/admin/app/toss" element={<Toss />} />
 
                     {/* Management */}
                     <Route
